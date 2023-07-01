685fbd2625c33675c45257a7a96613eb7d5385e7
Mottey Amos <akamottey@st.ug.edu.gh>
2023-07-01 23:24:56 +0000

    close bar

--- blog/src/components/Header.jsx
+++ blog/src/components/Header.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { images } from "../constants";
-
+import { AiOutlineMenu,AiOutlineClose} from 'react-icons/ai';
 const navItemsInfo = [
     {name: "Home"},
     {name: "Articles"},
@@ -21,13 +22,20 @@ const NavItem = ({name}) => {
   );
 };
 const Header = () => {
+  const [navIsvisible, setNavIsVisible] = useState(false);
+
+  const navVisibilityHandler = (curState)=>{
+    return !curState;
+  }
   return (
     <section>
       <header className="container mx-auto px-5 py-5 flex justify-between">
         <div>
           <img src={images.logo} alt="logo" />
         </div>
-        <div className="flex gap-x-9 items-center">
+        <div>{navIsvisible ? (<AiOutlineClose className="w-6 h-6" onClick={navVisibilityHandler}/>) :(<AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler}/>)}</div>
+        <div className={`${navIsvisible? "right-0 ": " "} flex fixed -right-full justify-center w-full lg:w-auto lg:justify-end bottom-0 top-0 gap-x-9 items-center lg:static flex-col`}
+         >
           <ul className="flex gap-x-5 font-semibold">
             {
                 navItemsInfo.map((item, key)=>{
